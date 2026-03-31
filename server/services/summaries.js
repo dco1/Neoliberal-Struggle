cfbfbe60bdbbcb8a47c9b3dbc0ed84fadcb8a9f2
Danny <danny@dannycohen.design>
2026-03-31 13:55:12 -0700

    Update summaries.js

--- server/services/summaries.js
+++ server/services/summaries.js
@@ -28,7 +28,7 @@ let ollamaClient = null;
 async function getClient() {
   if (!ollamaClient) {
     ollamaClient = {
-      messages(params) {
+      async messages(params) {
         const body = {
           model: SUMMARY_MODEL,
           stream: false,
